6cb05bdcaae5b6d72de662a66afe381bef3d8990
hask777 <haskellisp@gmail.com>
2022-11-28 20:20:12 +0300

    f

--- flats.py
+++ flats.py
@@ -6,95 +6,53 @@ import pandas as pd
 import os.path
 
 url = "https://moyareklama.by/Гомель/квартиры_продажа/"
-page = requests.get(url)
-page = page.text
+data = requests.get(url)
 
-# path = os.path.exists('html/flats.html')
 
-# if path != True:
-#     with open("html/flats.html", "w", encoding="utf-8") as f:
-#         f.write(page)
-# else:
-#     print("this file allready exists!")
+# with open("html/flats.html", "w", encoding="utf-8") as f:
+#         f.write(data.text)
 
 # with open("html/flats.html", 'r', encoding='utf-8') as f:
 #     page = f.read()
-# print(data)
 
-soup = BeautifulSoup(page, 'html5lib')
+soup = BeautifulSoup(data.text, 'lxml')
+# print(soup.prettify)
+
 items = soup.find_all('div', class_="one_advert_list")
-
-main_url = 'https://www.moyareklama.by/'
-
+# print(len(items))
 app_dict = {}
 app_arr = []
 
+num = 0
+
 for item in items:
+    num = num + 1
+    item_id = item.find('div', class_="title").find('a').get('href').replace('/single/ad/', '')
+    link = item.find('div', class_="title").find('a').get('href')
+    title = item.find('div', class_="title").text
+    address = item.find('div', class_="address").text
+    price = item.find('div', class_="price_block").text
     try:
-        item_id = item.find('div', class_='block-for-slider').get('rel')
+        company_link = item.find('div', class_="company").find('a').get('href')
     except:
         continue
+    company_name = item.find('div', class_="company").text
     
-    try:
-        title = item.find('div', class_='title').text
-    except:
-        continue
-
-    try:
-        # single_link = item.find('a', class_="premium").get('href')
-        single_link = item.find_all('a')[0].get('href')
-    except:
-        # single_link = item.find_all('a')[0].get('href')
-        continue
-
-    try:
-        address = item.find('div', class_='address').text
-    except:
-        continue
-
-    try:
-        price = item.find('div', class_='price').text
-    except:
-        continue
-
-    try:
-        price_info = item.find('div', class_='price_info').text
-    except:
-        continue
-
-    try:
-        company_link = item.find('a', class_='realty_link').get('href')
-    except:
-        continue
-
-    try:
-        company_name = item.find('a', class_='realty_link').text
-    except:
-        continue
-    
-    try:
-        date = item.find('div', class_='date').text
-    except:
-        continue
-
-    print(item_id)
-    app_dict = {
-                    'id': item_id,
-                    'title': title,
-                    'single_link': single_link,
-                    'address': address,
-                    'price': price,
-                    'price_info': price_info,
-                    'company_link': company_link,
-                    'company_name': company_name,
-                    'date': date
-                }
-
+    app_dict ={
+        'number': num,
+        'id': item_id,
+        'link': link,
+        'title': title,
+        'address': address,
+        'price': price,
+        'company_link': company_link,
+        'company_name': company_name
+    }
 
     app_arr.append(app_dict)
 
 with open('json/flats.json', 'w', encoding='utf-8') as f:
     json.dump(app_arr, f, ensure_ascii = False, indent =4, sort_keys=False)
 
-df = pd.read_json('json/flats.json')
-df.to_csv('csv/flats.csv')
+
+# print(app_arr)

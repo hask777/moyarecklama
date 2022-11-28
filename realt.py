7018767c538bc20566f5f43f9b6cf5c7f886cbe3
hask777 <haskellisp@gmail.com>
2022-11-28 14:50:29 +0300

    flats

--- realt.py
+++ realt.py
@@ -3,13 +3,24 @@ from bs4 import BeautifulSoup
 import html5lib
 import json
 import pandas as pd
+import os.path
 
 url = "https://moyareklama.by/Гомель/новостройки/"
-req = requests.get(url)
-data = req.text
+data = requests.get(url)
+
+path = os.path.exists('html/realt.html')
+
+if path != True:
+    with open("html/realt.html", "w", encoding="utf-8") as f:
+        f.write(data.text)
+else:
+    print("this file allready exists!")
+
+with open("html/realt.html", 'r', encoding='utf-8') as f:
+    page = f.read()
 # print(data)
 
-soup = BeautifulSoup(data, 'html5lib')
+soup = BeautifulSoup(page, 'html5lib')
 items = soup.find_all('div', class_="one_complex_list")
 
 main_url = 'https://www.moyareklama.by/'
